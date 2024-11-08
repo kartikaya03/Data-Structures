b519d19d52256ee448ef45cdf46ccdeaf19b6a63
Kartikaya Gupta <118833460+kartikaya03@users.noreply.github.com>
2024-11-08 21:10:34 +0530

    easy level leetcode problems

---
+++ DSAPractice/src/Arrays/easy/concat.java
@@ -0,0 +1,32 @@
+package Arrays.easy;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class concat {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+         int [] nums  = new int [size];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int[] result = concatination(nums);
+        System.out.println(Arrays.toString(result));
+
+    }
+    public static int[] concatination(int[] nums) {
+
+        int[] result = new int[2 * nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = nums[i];
+            result[i + nums.length] = nums[i];
+        }
+        return result;
+    }
+}
+
+
---
+++ DSAPractice/src/Arrays/easy/permutation.java
@@ -0,0 +1,24 @@
+package Arrays.easy;
+
+import java.util.Arrays;
+
+public class permutation {
+    public static void main(String[] args) {
+       int [] nums = {0,2,1,5,3,4};
+
+       int [] ans = buildArray(nums);
+        System.out.println(Arrays.toString(ans));
+
+    }
+    static int [] buildArray(int [] nums){
+        int [] result = new int [nums.length];
+
+        for(int i =0; i < nums.length;i++){
+            result[i]= nums[nums[i]];
+        }
+        return result;
+    }
+}
+
+
+
