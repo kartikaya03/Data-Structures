7b8e29ae3b261fa41017ff3ebdcbbed4d4bf9fe1
Kartikaya Gupta <118833460+kartikaya03@users.noreply.github.com>
2024-11-13 22:52:47 +0530

    added easy level problem

---
+++ DSAPractice/src/arrays/easy/NumberOfGoodPairs.java
@@ -0,0 +1,24 @@
+package arrays.easy;
+
+public class NumberOfGoodPairs {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 1, 1, 3};
+        int ans = numIdenticalPairs(nums);
+        System.out.println(ans);
+    }
+
+    static int numIdenticalPairs(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = 0; j < nums.length; j++) {
+
+
+                if (nums[i] == nums[j] && i < j) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
