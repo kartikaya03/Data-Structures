6a176f07f734977d59f86a9aab5daa4a8daf5bf7
Kartikaya Gupta <118833460+kartikaya03@users.noreply.github.com>
2024-11-16 02:42:46 +0530

    added easy level problem

---
+++ DSAPractice/src/arrays/easy/TragetArrayInGivenArray.java
@@ -0,0 +1,37 @@
+package arrays.easy;
+
+import java.util.Arrays;
+
+public class TragetArrayInGivenArray {
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 2, 3, 4};
+        int[] index = {0, 1, 2, 2, 1};
+
+        Solution solution = new Solution();
+        int[] result = solution.createTargetArray(nums, index);
+        System.out.println(Arrays.toString(result));
+    }
+
+}
+class Solution {
+    public int[] createTargetArray(int[] nums, int[] index) {
+        int[] target = new int[nums.length];
+
+        // Shift elements and insert values
+        for (int i = 0; i < nums.length; i++) {
+            int insertIndex = index[i];
+
+            // Shift elements to the right from the insertIndex
+            for (int j = nums.length - 1; j > insertIndex; j--) {
+                target[j] = target[j - 1];
+            }
+
+            // Insert the current element
+            target[insertIndex] = nums[i];
+        }
+
+        return target;
+    }
+}
+
+
