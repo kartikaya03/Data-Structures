095a068d7950a7d19d32f07709b77b17199a63f9
Kartikaya Gupta <118833460+kartikaya03@users.noreply.github.com>
2025-01-30 03:38:11 +0530

    a new prob

---
+++ DSAPractice/src/RandomProblems/ReverseArray.java
@@ -0,0 +1,15 @@
+package RandomProblems;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int [] arr = {12,10,20,80,70};
+
+        System.out.println("Reverse Array :");
+        for (int i = arr.length-1; i >=0 ; i--) {
+
+            System.out.print(" "+arr[i]);
+
+        }
+
+    }
+}
