74d8c15451846c2362511c6e75d3c690c3f54079
Kartikaya Gupta <118833460+kartikaya03@users.noreply.github.com>
2025-01-30 02:26:04 +0530

    oops concepts and notes reference added

---
+++ DSAPractice/src/RandomProblems/ArrayList.java
@@ -0,0 +1,21 @@
+package RandomProblems;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+public class ArrayList {
+
+    public static void main(String[] args) {
+
+
+
+
+        List<String> list = Arrays.asList("a", "b", "c", "d");
+
+
+            System.out.println(list);
+        }
+
+    }
+
---
+++ DSAPractice/src/RandomProblems/largestNumber.java
@@ -0,0 +1,17 @@
+package RandomProblems;
+
+public class largestNumber {
+    public static void main(String[] args) {
+        int [] arr = {90,12,23,42,87,124};
+        int maxNo = arr[0];
+
+
+        for (int i = 0; i < arr.length ; i++) {
+            if(maxNo<=arr[i]){
+                maxNo = arr[i];
+            }
+        }
+        System.out.println(maxNo);
+
+    }
+}
