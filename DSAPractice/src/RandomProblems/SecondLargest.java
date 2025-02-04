321ec5110406bfaefc455a3dcb3581922b3abdf8
Kartikaya Gupta <118833460+kartikaya03@users.noreply.github.com>
2025-02-04 22:42:59 +0530

    a new prob

--- DSAPractice/src/RandomProblems/SecondLargest.java
+++ DSAPractice/src/RandomProblems/SecondLargest.java
@@ -3,16 +3,38 @@ package RandomProblems;
 public class SecondLargest {
     public static void main(String[] args) {
         int[] arr = {10, 20, 100, 40, 60};
-        int secondMax = arr[0];
+        secondLargest(arr);
 
-        for (int i = 0; i < arr.length ; i++) {
-            if() {
-
-                secondMax= arr[i];
-            }
-        }
-
-        System.out.println(secondMax);
 
     }
+
+    static void secondLargest(int [] arr ){
+
+        int largest = Integer.MIN_VALUE;
+        int second  =  Integer.MIN_VALUE;
+
+
+        for (int i = 0; i < arr.length ; i++) {
+            if(arr[i]>largest){
+                second = largest;
+                largest = arr[i];
+            }else if (arr[i] > second && arr[i] != largest) {
+                second = arr[i];
+            }
+
+        }
+        System.out.println("Largest: " + largest);
+        System.out.println("Second Largest: " + second);
+
+
+
+
+
+
+
+    }
+
+
+
+
 }
---
+++ DSAPractice/src/RandomProblems/zeroes.java
@@ -0,0 +1,28 @@
+package RandomProblems;
+
+public class zeroes {
+    public static void main(String[] args) {
+ int [] arr = {0,1,0,3,12};
+
+        shiftZeroesToEnd(arr);
+
+        // Print the result
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void shiftZeroesToEnd(int[] arr) {
+        int j = 0; // Pointer for non-zero elements
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) { // If not zero, swap with the first zero found
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j++;
+            }
+        }
+    }
+
+}
