9406b60712bd8848699ec94269680df7234b8266
Kartikaya Gupta <118833460+kartikaya03@users.noreply.github.com>
2025-02-28 02:52:06 +0530

    1 problem solved

---
+++ DSAPractice/src/RandomProblems/arrays/CountPairs.java
@@ -0,0 +1,25 @@
+package RandomProblems.arrays;
+
+public class CountPairs {
+    public static void main(String[] args) {
+        int [] nums = {1,5,3,4,2};
+        int diff = 3;
+        CountPairs obj = new CountPairs();
+        obj.countPairs(nums,diff);
+
+    }
+
+    void countPairs(int[] nums, int diff) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (Math.abs(nums[i] - nums[j]) == diff) {
+                    count++;
+
+                }
+            }
+
+        }
+        System.out.println(count);
+    }
+}
